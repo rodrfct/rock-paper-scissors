54dd32fdf6c91edb1f9f4407436176122113fccb
rodrfct <rodrigoca.fct@gmail.com>
2023-09-14 10:51:44 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TheHeader from './components/TheHeader.vue';
+import Rules from './components/Rules.vue';
+import TheGame from './components/TheGame.vue';
+</script>
+
+<template>
+  <TheHeader />
+
+  <TheGame />
+
+  <Rules />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+    import Score from './components/Score.vue';
+</script>
+
+<template></template>
